29dd7a1096c5d19a2e8bbbac052c1bb6c828b847
geenafong <geenafong@gmail.com>
2018-11-24 18:40:47 -0800

    changed config.js

--- routes/books.js
+++ routes/books.js
@@ -6,16 +6,36 @@ var Book = require("../models").Book;
 /* GET books from the database. */
 router.get('/', function(req, res, next) {
     Book.findAll({order: [["title", "ASC"]]}).then(function(books){
-      res.render("index", {books: books});
+      res.render("index", {books});
     }).catch(function(error){
         res.send(500, error);
      });
   });
 
 /* Create a new book entry. */
-router.get('/books/new', function(req, res, next) {
-    res.render("/books/new", {book: Book.build()});
+router.get('/new', function(req, res, next) {
+    res.render("new-book", {book: Book.build()});
   });
+
+
+/*  Posts a new book to the database */
+router.post('/', function(req, res, next) {
+    Book.create(req.body).then(function(book) {
+      console.log(req.body)
+      res.redirect("/books");
+    }).catch(function(error){
+        if(error.name === "SequelizeValidationError") {
+          res.render("new-book", {book: Book.build(req.body), errors: error.errors})
+        } else {
+          throw error;
+        }
+    }).catch(function(error){
+        res.send(500, error);
+     });
+  ;});
+
+
+/* Shows book detail form */
 
 
 
