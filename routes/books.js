693ca0b5ab378c14bc1b9fe0c1aeb044a08ce88c
geenafong <geenafong@gmail.com>
2018-11-25 20:36:51 -0800

    update book

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
 var Book = require("../models").Book;
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-/* GET books from the database. */
+/* GET /books - Shows the full list of books */
 router.get('/', function(req, res, next) {
     Book.findAll({order: [["title", "ASC"]]}).then(function(books){
       res.render("index", {books});
@@ -12,31 +15,64 @@ router.get('/', function(req, res, next) {
      });
   });
 
-/* Create a new book entry. */
+/* GET /books/new - Shows the create new book form. */
 router.get('/new', function(req, res, next) {
     res.render("new-book", {book: Book.build()});
   });
 
 
-/*  Posts a new book to the database */
+/*  POST /books/new - Posts a new book to the database */
 router.post('/', function(req, res, next) {
     Book.create(req.body).then(function(book) {
-      console.log(req.body)
-      res.redirect("/books");
+      res.redirect('books/' + book.id);
     }).catch(function(error){
         if(error.name === "SequelizeValidationError") {
-          res.render("new-book", {book: Book.build(req.body), errors: error.errors})
+          res.render("new-book", {book: Book.build(req.body), errors: error.errors, title: "New Book"})
         } else {
           throw error;
         }
     }).catch(function(error){
         res.send(500, error);
      });
-  ;});
+  });
 
 
-/* Shows book detail form */
+/* GET /books/:id - Shows book detail form */
+router.get("/:id", function(req, res, next){
+    Book.findByPk(req.params.id).then(function(book){
+      if(book) {
+        res.render("update-book", {book: book, title: book.title});      
+      } else {
+        res.send(404);
+      }
+    }).catch(function(error){
+        res.send(500, error);
+     });
+  });
 
+  /* POST /books/:id - Updates book info in the database */
+  router.post("/:id", function(req, res, next){
+    console.log(req.params.id)
+    Book.findById(req.params.id).then(function(book){
+      if(book) {
+        return book.update(req.body);
+      } else {
+        res.send(404);
+      }
+    }).then(function(book){
+      res.redirect("/books/" + book.id);        
+    }).catch(function(error){
+        if(error.name === "SequelizeValidationError") {
+          var book = Book.build(req.body);
+          book.id = req.params.id;
+          res.render("books/edit", {book: book, errors: error.errors})
+        } else {
+          throw error;
+        }
 
+    }).catch(function(error){
+        res.send(500, error);
+     });
+  });
 
   module.exports = router;
